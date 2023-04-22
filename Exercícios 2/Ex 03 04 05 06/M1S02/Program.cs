e6869d3ff1c24e272cbf667ca0c88b347c173f54
Fernando da Cunha Ocampo More <131168072+Fermoree@users.noreply.github.com>
2023-04-21 23:31:54 -0300

    Add files via upload

---
+++ Exercícios 2/Ex 03 04 05 06/M1S02/Program.cs
@@ -0,0 +1,17 @@
+﻿using M1S02.DadosBancarios;
+using M1S02.DadosBancarios.Enum;
+
+ContaBancaria conta = new ContaBancaria();
+conta.numero = 1805;
+conta.agencia = 0301;
+conta.nomeDoTitular = "Fernando da Cunha Ocampo More";
+conta.tipoConta = TipoContaEnum.POUPANCA;
+
+// testando método depositar
+conta.Depositar(100);
+
+// testando método sacar 
+conta.Sacar(20);
+
+// testando método exibir saldo
+conta.ExibirSaldo();
