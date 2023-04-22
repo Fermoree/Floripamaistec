a5dd9b0b98cd43c1e9d223adf746c79490d4623e
Fernando da Cunha Ocampo More <131168072+Fermoree@users.noreply.github.com>
2023-04-21 23:35:55 -0300

    Add files via upload

---
+++ Semana 5 ex_002/ex_002/Program.cs
@@ -0,0 +1,26 @@
+﻿FileInfo arquivo = null;
+
+try
+{
+
+    Console.Write("Digite o nome do arquivo que você irá criar:  ");
+    string nomeArquivo = Console.ReadLine();
+
+    arquivo = new FileInfo(nomeArquivo);
+
+    using (StreamWriter gravadorFluxo = File.AppendText(nomeArquivo))
+    {
+        gravadorFluxo.WriteLine("Texto de dentro do arquivo");
+    }
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ocorreu o erro: {ex.Message}");
+}
+
+finally
+{
+    Console.WriteLine("Encerrando programa.");
+    arquivo = null;
+}
